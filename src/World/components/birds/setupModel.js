13ef540ba2100edf8225a7b36d527ce749d0a0d8
dracero <diego.racero@gmail.com>
2024-03-09 15:00:52 -0300

    vr ready

--- src/World/components/birds/setupModel.js
+++ src/World/components/birds/setupModel.js
@@ -1,20 +1,5 @@
 import { AnimationMixer } from "three";
-
-//Esta function es solo para seleccionar un hijo del modelo
-/*function setupModel(data) {
-    const model = data.scene.children[0];
-    const clip = data.animations[0];
-    const mixer = new AnimationMixer(model);
-    const action = mixer.clipAction(clip);
-    action.play();  
-
-    model.tick = (delta) => mixer.update(delta);
-  
-    return model;
-  }
-  
-  export { setupModel };*/
-  
+ 
   //Esta function es para seleccionar el modelo completo y sus animaciones
   class AnimationModel {
     constructor(data) {
--- src/World/systems/renderer.js
+++ src/World/systems/renderer.js
@@ -1,10 +1,13 @@
 import { WebGLRenderer } from 'three';
+import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 function createRenderer() {
   const renderer = new WebGLRenderer({ antialias: true });
 
   renderer.physicallyCorrectLights = true;
-
+  document.body.appendChild( VRButton.createButton( renderer ) );
+  renderer.xr.enabled = true;
+  
   return renderer;
 }
 
