61df7f051beec7a26cdeac6b925366dcedb4dae1
dracero <diego.racero@gmail.com>
2024-04-17 16:21:57 -0300

    size

--- src/World/components/birds/birds.js
+++ src/World/components/birds/birds.js
@@ -14,7 +14,7 @@ async function loadSpheres() {
   const sphere = new AnimationModel(sphereData);
   sphere.setupModel();
   sphere.model.position.set(0, 0, 0); // Centra el objeto
-  sphere.model.scale.set(0.05, 0.05, 0.05); // Ajusta el tamaño del objeto
+  sphere.model.scale.set(0.6, 0.6, 0.6); // Ajusta el tamaño del objeto
   
   return {
     sphere,
