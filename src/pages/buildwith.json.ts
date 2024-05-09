b16fa35ec1777d7c1773ed611f03f9cb693d6ed0
dracero <dracero@fi.uba.ar>
2024-05-09 23:00:26 +0000

    file

--- src/pages/buildwith.json.ts
+++ src/pages/buildwith.json.ts
@@ -1,4 +1,4 @@
-// @ts-nocheck
+/*// @ts-nocheck
 import Counter from "./db.js";
 
 //Función de escritura
@@ -29,6 +29,42 @@ export async function GET() {
   }
   // Devolver la respuesta
   return new Response(JSON.stringify(data), {
+    status: 200,
+  });
+}*/
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import fs from "fs/promises";
+
+export async function POST({ request }) {
+  const body = await request.json();
+  const count = body.counter;
+
+  // Obtener la ruta del directorio del módulo actual
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  // Definir la ruta del archivo en el directorio público
+  const dirPath = path.join(__dirname, "public");
+  const filePath = path.join(dirPath, "esfera.json");
+
+  // Crear el directorio si no existe
+  try {
+    await fs.mkdir(dirPath, { recursive: true });
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Guardar el valor en el archivo en lugar de en la base de datos
+  try {
+    await fs.writeFile(filePath, JSON.stringify(count));
+    console.log(`Archivo guardado en: ${filePath}`);
+  } catch (err) {
+    console.error(err);
+  }
+
+  // Devolver la respuesta
+  return new Response(JSON.stringify(count), {
     status: 200,
   });
 }
