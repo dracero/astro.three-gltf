8f0fe70329c7fb7bd4039cc5f0bc9e45dd68604d
dracero <dracero@fi.uba.ar>
2024-05-18 22:44:21 +0000

    movement

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,27 +1,30 @@
-import { World } from '../World/Wolrd.js';
+import { World } from "../World/Wolrd.js";
 
-  async function Main() {
-		// Get a reference to the container element
-		const container = document.querySelector('#scene-container');
-		// create a new world
-    const world = new World(container); // Pasa xrRenderer en lugar de xrSession
-        // complete async tasks
-        await world.init();
-        // start the animation loop
-        world.start();
-        /*document.getElementById('hud').addEventListener('click', function() {
-        world.move()});
-        document.getElementById('hud2').addEventListener('click', function() {
-        world.pause()});
-        document.getElementById('hud3').addEventListener('click', function() {
-        world.playBack()});
-        document.getElementById('hud4').addEventListener('click', function() {
+async function Main() {
+  // Get a reference to the container element
+  const container = document.querySelector("#scene-container");
+  // create a new world
+  const world = new World(container); // Pasa xrRenderer en lugar de xrSession
+  // complete async tasks
+  await world.init();
+  // start the animation loop
+  world.start();
+  document.getElementById("hud").addEventListener("click", function () {
+    world.move();
+  });
+  document.getElementById("hud2").addEventListener("click", function () {
+    world.pause();
+  });
+  document.getElementById("hud3").addEventListener("click", function () {
+    world.playBack();
+  });
+  /*document.getElementById('hud4').addEventListener('click', function() {
         world.eraseSphere()});*/
-        world.starXR()
-    } 
+  world.starXR();
+}
 
-    Main().catch((err) => {
-      console.error(err);
-    });
+Main().catch((err) => {
+  console.error(err);
+});
 
-export default Main
+export default Main;
